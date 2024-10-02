9b0b06b20469c42001fbb4710f95d66f5c14f84e
Pratik <PratikUgile1371@gmail.com>
2024-10-02 20:52:13 +0530

    Changes in razorpaypaymentgateway

---
+++ digitalmaharaja/Secured/Razorpaypaymentgateway.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Razorpaypaymentgateway.aspx.cs" Inherits="Secured_Razorpaypaymentgateway" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btntest" runat="server" OnClick="btntest_Click" Text="Test" />
+        </div>
+    </form>
+    <label>R</label>
+    <form name="razorpayForm">
+        <input id="razorpay_payment_id" type="hidden" name="razorpay_payment_id" />
+        <input id="razorpay_order_id" type="hidden" name="razorpay_order_id" />
+        <input id="razorpay_signature" type="hidden" name="razorpay_signature" />
+    </form>
+    <button id="rzp-button1">Pay with Razorpay</button>
+    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+    <script>
+        var orderId = "<%=Session["orderid"]%>";
+        var options = {
+            "key": "rzp_test_jOgZV0Qn2un96C", // Enter the Key ID generated from the Dashboard
+            "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+            "currency": "INR",
+            "name": "Acme Corp",
+            "description": "Buy Green Tea",
+            "order_id": orderId,
+            "image": "https://example.com/your_logo",
+            "prefill": {
+                "name": "Gaurav Kumar",
+                "email": "gaurav.kumar@example.com",
+                "contact": "919000090000",
+            },
+            "notes": {
+                "address": "Hello World"
+            },
+            "theme": {
+                "color": "#3399cc"
+            }
+        }
+        // Boolean whether to show image inside a white frame. (default: true)
+        options.theme.image_padding = false;
+        options.handler = function (response) {
+            document.getElementById('razorpay_payment_id').value = response.razorpay_payment_id;
+            document.getElementById('razorpay_order_id').value = orderId;
+            document.getElementById('razorpay_signature').value = response.razorpay_signature;
+            document.razorpayForm.submit();
+        };
+        options.modal = {
+            ondismiss: function () {
+                console.log("This code runs when the popup is closed");
+            },
+            // Boolean indicating whether pressing escape key 
+            // should close the checkout form. (default: true)
+            escape: true,
+            // Boolean indicating whether clicking translucent blank
+            // space outside checkout form should close the form. (default: false)
+            backdropclose: false
+        };
+        var rzp = new Razorpay(options);
+        document.getElementById('rzp-button1').onclick = function (e) {
+            rzp.open();
+            e.preventDefault();
+        }
+
+    </script>
+</body>
+</html>
---
+++ digitalmaharaja/Secured/Razorpaypaymentgateway.aspx.cs
@@ -0,0 +1,113 @@
+﻿using Razorpay.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Secured_Razorpaypaymentgateway : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void orderID()
+    {
+        Dictionary<string, object> input = new Dictionary<string, object>();
+        input.Add("amount", 100); // this amount should be same as transaction amount
+        input.Add("currency", "INR");
+        input.Add("receipt", "12121");
+
+        string key = "rzp_test_jOgZV0Qn2un96C";
+        string secret = "a7GogPFubSiqawrfm7hALkb9";
+
+        RazorpayClient client = new RazorpayClient(key, secret);
+
+        Razorpay.Api.Order order = client.Order.Create(input);
+        Session["orderid"] = order["id"].ToString();
+    }
+    protected void test()
+    {
+        // Razorpay API Key and Secret
+        //string key = "rzp_test_jOgZV0Qn2un96C";
+        //string secret = "a7GogPFubSiqawrfm7hALkb9";
+
+        //Dictionary<string, object> input = new Dictionary<string, object>();
+        //input.Add("amount", 100); // this amount should be same as transaction amount
+        //input.Add("currency", "INR");
+        //input.Add("receipt", "12121");
+
+        //var client = new HttpClient();
+        ////var request = new HttpRequestMessage(Post, "https://api.razorpay.com/v1/orders");
+        //request.Headers.Add("Authorization", "Basic       ");
+        //var content = new StringContent("{\n  \"amount\": 1000000,\n  \"currency\": \"INR\",\n  \"receipt\": \"Receipt no. 1\",\n  \"notes\": {\n    \"notes_key_1\": \"Tea, Earl Grey, Hot\",\n    \"notes_key_2\": \"Tea, Earl Grey… decaf.\"\n  }\n}", null, "text/plain");
+        //request.Content = content;
+        //var response = client.SendAsync(request).Result;
+        //response.EnsureSuccessStatusCode();
+        //Console.WriteLine(response.Content.ReadAsStringAsync());
+        // Combine orderId and razorpayPaymentId
+        //string message = orderId + "|" + razorpayPaymentId;
+
+        //// Compute the HMAC SHA256
+        //string hmacHash = ComputeHMAC_SHA256(message, secretKey);
+
+        //public static string ComputeHMAC_SHA256(string message, string secret)
+        //{
+        //    // Convert the secret key and the message to byte arrays
+        //    byte[] keyBytes = Encoding.UTF8.GetBytes(secret);
+        //    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+        //    // Initialize HMACSHA256 with the secret key
+        //    using (var hmacsha256 = new HMACSHA256(keyBytes))
+        //    {
+        //        // Compute the HMAC SHA256 hash
+        //        byte[] hashBytes = hmacsha256.ComputeHash(messageBytes);
+
+        //        // Convert the hash bytes to a hexadecimal string
+        //        StringBuilder sb = new StringBuilder();
+        //        foreach (byte b in hashBytes)
+        //        {
+        //            sb.Append(b.ToString("x2")); // Convert each byte to a hexadecimal string
+        //        }
+
+        //        return sb.ToString(); // Return the final hash string
+        //    }
+        //}
+
+    }
+
+    protected void btntest_Click(object sender, EventArgs e)
+    {
+        orderID();
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Notes
+    {
+        public string notes_key_1 { get; set; }
+        public string notes_key_2 { get; set; }
+    }
+
+    public class Root
+    {
+        public int amount { get; set; }
+        public int amount_due { get; set; }
+        public int amount_paid { get; set; }
+        public int attempts { get; set; }
+        public int created_at { get; set; }
+        public string currency { get; set; }
+        public string entity { get; set; }
+        public string id { get; set; }
+        public Notes notes { get; set; }
+        public object offer_id { get; set; }
+        public string receipt { get; set; }
+        public string status { get; set; }
+    }
+
+
+}
